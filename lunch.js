e73c1ae94a379113dca12127d9a1c921186b3086
Erik Ahlgren <erik.ahlgren@teliacompany.com>
2020-07-06 19:12:24 +0200

    changed port

--- lunch.js
+++ lunch.js
@@ -2,7 +2,7 @@ const express = require('express')
 // const https = require('https')
 // const fs = require('fs')
 const app = express()
-const port = 3000
+const port = 8888
 const token = process.env.TOKEN
 
 const axios = require('axios').create({
