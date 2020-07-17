4ed6dae81317736f9aa07e2ee5bf4edbf54ba820
Erik Ahlgren <erik.ahlgren@teliacompany.com>
2020-07-17 16:37:42 +0200

    changed port

--- lunch.js
+++ lunch.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const express = require('express')
-// const https = require('https')
-// const fs = require('fs')
 const app = express()
-const port = 8888
+const port = 3000
 
 const token = process.env.TOKEN
 
@@ -17,18 +15,11 @@ const axios = require('axios').create({
   }
 })
 
-// const privateKey = fs.readFileSync('./cert/server.key', 'utf8');
-// const certificate = fs.readFileSync('./cert/server.crt', 'utf8');
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate
-//   };
-// const httpsServer = https.createServer(credentials, app);
-// httpsServer.listen(port);
 app.listen(port, () => console.log(`Service listening at http://localhost:${port}`))
 
 app.post('/lunch', (_, response) => {
   
+  // Get unix timestamp one hour from now
   const exp = new Date().setHours(new Date().getHours() + 1) / 1000
   
   const data = {
